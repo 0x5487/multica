99c08f2fe7b7688775eaa3b105479cdefb08613d
0x5487 <jawc@hotmail.com>
2026-04-12 16:46:12 +0800

    feat(svelte-web): implement IssueList component using shadcn-svelte Table

---
+++ apps/svelte-web/src/lib/components/issues/IssueList.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import * as Table from "$lib/components/ui/table";
+  import { Badge } from "$lib/components/ui/badge";
+
+  let { issues = [] } = $props();
+</script>
+
+<Table.Root>
+  <Table.Header>
+    <Table.Row>
+      <Table.Head>Title</Table.Head>
+      <Table.Head>Status</Table.Head>
+      <Table.Head>Priority</Table.Head>
+    </Table.Row>
+  </Table.Header>
+  <Table.Body>
+    {#each issues as issue (issue.id)}
+      <Table.Row>
+        <Table.Cell class="font-medium">{issue.title}</Table.Cell>
+        <Table.Cell><Badge variant="outline">{issue.status}</Badge></Table.Cell>
+        <Table.Cell>{issue.priority}</Table.Cell>
+      </Table.Row>
+    {/each}
+  </Table.Body>
+</Table.Root>
